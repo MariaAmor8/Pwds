0e89a43eb345a69234eb9cba810a7e59f641fd89
María José Amorocho <123780212+MariaAmor8@users.noreply.github.com>
2023-11-30 10:50:59 -0500

    interfaz

--- Pwds/src/FPrincipal/Principal.java
+++ Pwds/src/FPrincipal/Principal.java
@@ -1,10 +1,41 @@
 package FPrincipal;
 
-public class Principal {
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Principal extends JFrame{
+	JTextField txtUsuario;
+	JTextField txtContrasenia;
+	
+	public void ejecutar() {
+		this.setPreferredSize(new Dimension(350,250));
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.setLayout(new GridLayout(3,2));
+		
+		this.add(new JLabel("Nombre de usuario"));
+		this.txtUsuario = new JTextField();
+		this.add(txtUsuario);
+		this.add(new JLabel("Contraseña"));
+		this.txtContrasenia = new JTextField();
+		this.add(this.txtContrasenia);
+		
+		JButton bEviar = new JButton("Registrarse");
+		this.add(bEviar);
+		
+		
+		this.pack();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Principal principal = new Principal();
+		principal.ejecutar();
 	}
 
 }
